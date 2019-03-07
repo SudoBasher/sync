36866260e244411b79c7fed2456c6b14e500b6e3
sudobasher <email.ca@gmail.com>
2019-03-06 16:16:12 -0800

    Updating stock config file with lowercase names.

--- config.sh
+++ config.sh
@@ -3,31 +3,31 @@
 ## modify these settings as needed
 
 	## linux options
-	LinuxWorkstationHomeDirectory="/home/LOCALUSERNAMEHERE/"
+	LinuxWorkstationHomeDirectory="/home/user/"
 
 	## mac options
 	MacWorkstationHomeDirectory="/Users/LOCALUSERNAMEHERE/"
 
 	## global settings
-	LocalExcludeFileDirectory="Development/Github/sudobasher/sync/"
-	LocalDocumentsDirectory="Documents/"
-	LocalDevelopmentUploadDirectory="Development/"
-	LocalStorageUploadDirectory="Storage/"
-	LocalStorageDownloadDirectory="Storage-Download/"
-	SecureDirectory="Secure/"
+	LocalExcludeFileDirectory="development/github/sudobasher/sync/"
+	LocalDocumentsDirectory="documents/"
+	LocalDevelopmentUploadDirectory="development/"
+	LocalStorageUploadDirectory="storage/"
+	LocalStorageDownloadDirectory="storage/"
+	SecureDirectory="secure/"
 
-	LocalStorageHostsFile="Software/Configuration Files/workstation_hosts.txt"
+	LocalStorageHostsFile="software/configuration files/workstation_hosts.txt"
 	LocalSystemHostsFile="/etc/hosts"
-	LocalStorageSSHConfigFile="Software/Configuration Files/workstation_ssh_config.txt"
+	LocalStorageSSHConfigFile="software/configuration files/workstation_ssh_config.txt"
 	LocalSystemSSHConfigFile=".ssh/config"
 
-	RemoteServer="sync2"
-	RemoteStorageDirectory="/media/ssd60/"
+	RemoteServer="storage1"
+	RemoteStorageDirectory="/home/sudobasher/storage/"
 	RedundantServer="sync3"
 	RedundantStorageDirectory="/media/ssd60/"
 	NASServer="airlock"
-	NASStorageDirectory="/volume1/Backup\ Data/Storage/"
-	NASDevelopmentDirectory="/volume1/Backup\ Data/Development/"
+	NASStorageDirectory="/volume1/Backup\ Data/storage/"
+	NASDevelopmentDirectory="/volume1/Backup\ Data/development/"
 
 ## keep these system settings as is
 ## messing with some of these could give you a serious headache
